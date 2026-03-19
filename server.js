7b6e3578ab0f3a05e71e082e0d64ea10b5d57d32
yeadr <yussef.elalami@institutvidreres.cat>
2026-03-19 16:44:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ const google = require("googleapis")
 const PORT = process.env.PORT || 3000;
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')  
-  })
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CREDENTIALS))
 });
 
 const db = admin.firestore();
